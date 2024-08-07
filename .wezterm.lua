850d611e8a01c0f165607fcf1b57987fe5f6869b
Björn Martensen <b.martensen@zw-systems.de>
2024-08-07 10:04:13 +0200

    add wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,39 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Frappe"
+
+-- config.font = wezterm.font("MesloLGL Nerd Font Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+config.font_size = 16
+config.line_height = 1.0
+
+config.enable_tab_bar = false
+
+config.window_decorations = "TITLE | RESIZE"
+
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+
+config.font_rules = {
+	{
+		intensity = "Bold",
+		italic = false,
+		font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Bold", stretch = "Normal", style = "Normal" }),
+	},
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Bold", stretch = "Normal", style = "Italic" }),
+	},
+}
+
+-- keep adding configuration options here
+--
+
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = false
+
+return config
