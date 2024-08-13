176a8411fdfa97cbd3f815124cdb6c83c6b7715c
Björn Martensen <b.martensen@zw-systems.de>
2024-08-13 17:22:34 +0200

    update vim config

--- nvim/.config/nvim/lua/plugins/linting.lua
+++ nvim/.config/nvim/lua/plugins/linting.lua
@@ -9,6 +9,7 @@ return {
 			typescript = { "eslint_d" },
 			javascriptreact = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
+			php = {}, -- Remove phpcs linter
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
