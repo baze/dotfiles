21bb805748ec1c7ee2dbf2723cc756a4804161ec
Björn Martensen <b.martensen@zw-systems.de>
2024-08-15 11:25:17 +0200

    remove custom pint installation

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -1,9 +1,7 @@
-local util = require("conform.util")
 return {
-	"stevearc/conform.nvim",
-	opts = function()
-		---@type conform.setupOpts
-		local opts = {
+	{
+		"stevearc/conform.nvim",
+		opts = {
 			formatters_by_ft = {
 				javascript = { "prettier" },
 				typescript = { "prettier" },
@@ -23,36 +21,16 @@ return {
 				php = { "pint" },
 				blade = { "blade-formatter" },
 			},
-			-- LazyVim will merge the options you set here with builtin formatters.
-			-- You can also define any custom formatters here.
-			---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-			formatters = {
-				injected = { options = { ignore_errors = true } },
-				-- # Example of using dprint only when a dprint.json file is present
-				-- dprint = {
-				--   condition = function(ctx)
-				--     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-				--   end,
-				-- },
-				--
-				-- # Example of using shfmt with extra args
-				-- shfmt = {
-				--   extra_args = { "-i", "2", "-ci" },
-				-- },
-				pint = {
-					meta = {
-						url = "https://github.com/laravel/pint",
-						description = "Laravel Pint is an opinionated PHP code style fixer for minimalists. Pint is built on top of PHP-CS-Fixer and makes it simple to ensure that your code style stays clean and consistent.",
-					},
-					command = util.find_executable({
-						vim.fn.stdpath("data") .. "/mason/bin/pint",
-						"vendor/bin/pint",
-					}, "pint"),
-					args = { "$FILENAME" },
-					stdin = false,
-				},
+		},
+	},
+	{
+		-- Remove phpcs linter.
+		"mfussenegger/nvim-lint",
+		optional = true,
+		opts = {
+			linters_by_ft = {
+				php = {},
 			},
-		}
-		return opts
-	end,
+		},
+	},
 }
