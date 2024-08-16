a8b0d92351b8b31ebd897ab4ef96d912a15241cf
Björn Martensen <b.martensen@zw-systems.de>
2024-08-16 08:05:40 +0200

    add more nvim configuration

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,34 +1,63 @@
 -- add more treesitter parsers
 return {
-	"nvim-treesitter/nvim-treesitter",
-	opts = {
-		ensure_installed = { "php", "javascript", "html", "css", "json", "typescript", "blade", "vue" },
-		auto_install = true,
-		highlight = {
-			enable = true,
-		},
-		-- Needed because treesitter highlight turns off autoindent for php files
-		indent = {
-			enable = true,
-		},
-	},
-	config = function(_, opts)
-		---@class ParserInfo[]
-		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-		parser_config.blade = {
-			install_info = {
-				url = "https://github.com/EmranMR/tree-sitter-blade",
-				files = { "src/parser.c" },
-				branch = "main",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"php",
+				"javascript",
+				"html",
+				"css",
+				"json",
+				"typescript",
+				"blade",
+				"vue",
 			},
-			filetype = "blade",
-		}
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+			-- Needed because treesitter highlight turns off autoindent for php files
+			indent = {
+				enable = true,
+			},
 
-		vim.filetype.add({
-			pattern = {
-				[".*%.blade%.php"] = "blade",
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
 			},
-		})
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+		},
+		config = function(_, opts)
+			---@class ParserInfo[]
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			parser_config.blade = {
+				install_info = {
+					url = "https://github.com/EmranMR/tree-sitter-blade",
+					files = { "src/parser.c" },
+					branch = "main",
+				},
+				filetype = "blade",
+			}
+
+			vim.filetype.add({
+				pattern = {
+					[".*%.blade%.php"] = "blade",
+				},
+			})
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 }
