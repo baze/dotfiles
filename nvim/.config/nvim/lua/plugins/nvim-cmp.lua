db00a047a413e9d2bcfdd1e86780c5f883e13774
Björn Martensen <b.martensen@zw-systems.de>
2024-08-26 13:05:43 +0200

    switch to dynamic width for nvim-cmp

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -23,10 +23,6 @@ return {
 
 		local lspkind = require("lspkind")
 
-		local ELLIPSIS_CHAR = "…"
-		local MIN_LABEL_WIDTH = 20
-		local MAX_LABEL_WIDTH = 30
-
 		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
 		require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -77,25 +73,52 @@ return {
 				-- 	ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 				-- 	show_labelDetails = true, -- show labelDetails in menu. Disabled by default
 				-- }),
-				fields = { "kind", "abbr", "menu" },
-				format = function(entry, vim_item)
-					vim_item.kind = lspkind.presets.default[vim_item.kind] or ""
-					vim_item.menu = ({
-						nvim_lsp = "[LSP]",
-						luasnip = "[LuaSnip]",
-						buffer = "[Buffer]",
-						path = "[Path]",
-						codium = "[Codeium]",
-					})[entry.source.name]
-					local label = vim_item.abbr
-					local truncated_label = vim.fn.strcharpart(label, 0, MAX_LABEL_WIDTH)
-					if truncated_label ~= label then
-						vim_item.abbr = truncated_label .. ELLIPSIS_CHAR
-					elseif string.len(label) < MIN_LABEL_WIDTH then
-						local padding = string.rep(" ", MIN_LABEL_WIDTH - string.len(label))
-						vim_item.abbr = label .. padding
+				-- fields = { "kind", "abbr", "menu" },
+				fields = { "abbr", "menu", "kind" },
+				format = function(entry, item)
+					-- item.kind = lspkind.presets.default[item.kind] or ""
+					-- Define menu shorthand for different completion sources.
+					local menu_icon = {
+						nvim_lsp = "NLSP",
+						nvim_lua = "NLUA",
+						luasnip = "LSNP",
+						buffer = "BUFF",
+						path = "PATH",
+					}
+					-- Set the menu "icon" to the shorthand for each completion source.
+					item.menu = menu_icon[entry.source.name]
+
+					-- Set the fixed width of the completion menu to 60 characters.
+					-- fixed_width = 20
+
+					-- Set 'fixed_width' to false if not provided.
+					fixed_width = fixed_width or false
+
+					-- Get the completion entry text shown in the completion window.
+					local content = item.abbr
+
+					-- Set the fixed completion window width.
+					if fixed_width then
+						vim.o.pumwidth = fixed_width
 					end
-					return vim_item
+
+					-- Get the width of the current window.
+					local win_width = vim.api.nvim_win_get_width(0)
+
+					-- Set the max content width based on either: 'fixed_width'
+					-- or a percentage of the window width, in this case 20%.
+					-- We subtract 10 from 'fixed_width' to leave room for 'kind' fields.
+					local max_content_width = fixed_width and fixed_width - 10 or math.floor(win_width * 0.2)
+
+					-- Truncate the completion entry text if it's longer than the
+					-- max content width. We subtract 3 from the max content width
+					-- to account for the "..." that will be appended to it.
+					if #content > max_content_width then
+						item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 3) .. "..."
+					else
+						item.abbr = content .. (" "):rep(max_content_width - #content)
+					end
+					return item
 				end,
 			},
 			window = {
