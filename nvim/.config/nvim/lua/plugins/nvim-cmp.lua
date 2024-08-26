7b5e7efcbc4c67348b5f297d1cb34cfb3acac04b
Björn Martensen <b.martensen@zw-systems.de>
2024-08-26 10:55:05 +0200

    nicer menus

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -23,12 +23,16 @@ return {
 
 		local lspkind = require("lspkind")
 
+		local ELLIPSIS_CHAR = "…"
+		local MAX_LABEL_WIDTH = 20
+		local MIN_LABEL_WIDTH = 20
+
 		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
 		require("luasnip.loaders.from_vscode").lazy_load()
 
 		cmp.setup({
 			completion = {
-				completeopt = "menu,menuone,preview,noselect",
+				completeopt = "menu,menuone,preview",
 			},
 			snippet = { -- configure how nvim-cmp interacts with snippet engine
 				expand = function(args)
@@ -58,23 +62,45 @@ return {
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
-				{ name = "path" }, -- file system paths
+				{ name = "path" },
 				{ name = "nvim_lsp", keyword_length = 1 },
-				{ name = "buffer", keyword_length = 3 }, -- text within current buffer
-				{ name = "luasnip", keyword_length = 2 }, -- snippets
+				{ name = "buffer", keyword_length = 3 },
+				{ name = "luasnip", keyword_length = 2 },
 				{ name = "codeium" },
 			}),
-
 			-- configure lspkind for vs-code like pictograms in completion menu
 			formatting = {
-				format = lspkind.cmp_format({
-					mode = "symbol_text", -- show only symbol annotations
-					maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-					-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-					ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-					show_labelDetails = false, -- show labelDetails in menu. Disabled by default
-				}),
-				fields = { "menu", "abbr", "kind" },
+				-- format = lspkind.cmp_format({
+				-- 	mode = "symbol", -- show only symbol annotations
+				-- 	maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+				-- 	-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+				-- 	ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+				-- 	show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+				-- }),
+				fields = { "kind", "abbr", "menu" },
+				format = function(entry, vim_item)
+					vim_item.kind = lspkind.presets.default[vim_item.kind] or ""
+					vim_item.menu = ({
+						nvim_lsp = "[LSP]",
+						luasnip = "[LuaSnip]",
+						buffer = "[Buffer]",
+						path = "[Path]",
+						codium = "[Codeium]",
+					})[entry.source.name]
+					local label = vim_item.abbr
+					local truncated_label = vim.fn.strcharpart(label, 0, MAX_LABEL_WIDTH)
+					if truncated_label ~= label then
+						vim_item.abbr = truncated_label .. ELLIPSIS_CHAR
+					elseif string.len(label) < MIN_LABEL_WIDTH then
+						local padding = string.rep(" ", MIN_LABEL_WIDTH - string.len(label))
+						vim_item.abbr = label .. padding
+					end
+					return vim_item
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
 			},
 		})
 	end,
