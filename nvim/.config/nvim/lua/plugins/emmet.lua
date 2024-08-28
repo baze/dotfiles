04c7537d829e4fde6b3226f2781ea9878f65e176
Björn Martensen <b.martensen@zw-systems.de>
2024-08-28 09:55:08 +0200

    cleaner emmet setup

--- nvim/.config/nvim/lua/plugins/emmet.lua
+++ nvim/.config/nvim/lua/plugins/emmet.lua
@@ -24,4 +24,15 @@ return {
 			},
 		},
 	},
+	{
+		"olrtg/nvim-emmet",
+		config = function()
+			vim.keymap.set(
+				{ "n", "v" },
+				"<leader>xe",
+				require("nvim-emmet").wrap_with_abbreviation,
+				{ silent = true, noremap = true, desc = "Wrap with abbreviation" }
+			)
+		end,
+	},
 }
