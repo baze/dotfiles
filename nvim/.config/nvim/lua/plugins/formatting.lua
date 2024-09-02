2f42e844967509b786ea8be1c8fb1502dd62102e
Björn Martensen <b.martensen@zw-systems.de>
2024-09-02 13:45:44 +0200

    re-add formatting and linting

---
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,24 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			css = { "prettier" },
+			graphql = { "prettier" },
+			handlebars = { "prettier" },
+			html = { "prettier" },
+			javascript = { "prettier" },
+			javascriptreact = { "prettier" },
+			json = { "prettier" },
+			liquid = { "prettier" },
+			lua = { "stylua" },
+			markdown = { "prettier" },
+			python = { "isort", "black" },
+			scss = { "prettier" },
+			svelte = { "prettier" },
+			typescript = { "prettier" },
+			typescriptreact = { "prettier" },
+			vue = { "prettier" },
+			yaml = { "prettier" },
+		},
+	},
+}
