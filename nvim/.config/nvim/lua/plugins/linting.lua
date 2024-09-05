643101effbcfece0840becd5b933315b88481ef7
Björn Martensen <b.martensen@zw-systems.de>
2024-09-05 15:10:58 +0200

    clean up linting

--- nvim/.config/nvim/lua/plugins/linting.lua
+++ nvim/.config/nvim/lua/plugins/linting.lua
@@ -1,10 +1,7 @@
 return {
 	"mfussenegger/nvim-lint",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local lint = require("lint")
-
-		lint.linters_by_ft = {
+	opts = {
+		linters_by_ft = {
 			javascript = { "eslint_d" },
 			javascriptreact = { "eslint_d" },
 			svelte = { "eslint_d" },
@@ -12,15 +9,6 @@ return {
 			typescript = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
 			vue = { "eslint_d" },
-		}
-
-		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-			group = lint_augroup,
-			callback = function()
-				lint.try_lint()
-			end,
-		})
-	end,
+		},
+	},
 }
