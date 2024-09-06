2c08f5c55605f8d5326d1c4da8f7a04d86e2df4a
Björn Martensen <b.martensen@zw-systems.de>
2024-09-06 09:16:49 +0200

    remove unused keymap

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,6 @@
 -- Add any additional keymaps here
 
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
