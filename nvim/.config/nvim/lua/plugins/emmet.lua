9a8e0d921df835559921a8c9bebe4fe747ba5c5f
Björn Martensen <b.martensen@zw-systems.de>
2025-02-25 09:37:36 +0100

    fix emmet language server (again)

--- nvim/.config/nvim/lua/plugins/emmet.lua
+++ nvim/.config/nvim/lua/plugins/emmet.lua
@@ -1,45 +1,37 @@
 return {
-	-- Mason-LSPConfig to bridge Mason and LSPConfig
 	{
-		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
 		opts = {
-			ensure_installed = { "emmet_language_server" },
-			automatic_installation = true,
+			-- @type lspconfig.options
+			servers = {
+				emmet_language_server = {
+					filetypes = {
+						"css",
+						"eruby",
+						"html",
+						"javascript",
+						"javascriptreact",
+						"less",
+						"sass",
+						"scss",
+						"pug",
+						"typescriptreact",
+						"blade",
+						"vue",
+					},
+				},
+			},
 		},
-		config = function()
-			require("mason-lspconfig").setup_handlers({
-				-- Default handler for all servers
-				function(server_name)
-					require("lspconfig")[server_name].setup({})
-				end,
-
-				-- Custom handler for emmet_language_server
-				["emmet_language_server"] = function()
-					require("lspconfig").emmet_language_server.setup({
-						filetypes = {
-							"html",
-							"css",
-							"scss",
-							"javascript",
-							"javascriptreact",
-							"typescriptreact",
-							"vue",
-							"blade",
-							"pug",
-							"less",
-						},
-						init_options = {
-							html = { options = { ["bem.enabled"] = true } },
-						},
-					})
-				end,
-			})
-		end,
 	},
 	{
 		"olrtg/nvim-emmet",
 		config = function()
-			vim.keymap.set({ "n", "v" }, "<leader>xe", require("nvim-emmet").wrap_with_abbreviation)
+			vim.keymap.set(
+				{ "n", "v" },
+				"<leader>xe",
+				require("nvim-emmet").wrap_with_abbreviation,
+				{ silent = true, noremap = true, desc = "Wrap with abbreviation" }
+			)
 		end,
 	},
 }
