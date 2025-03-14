35a2a29485f8954b2d2c188b8f776204fc1cfef9
Björn Martensen <b.martensen@zw-systems.de>
2025-03-14 07:00:55 +0100

    switch to blade from nvim-treesitter

--- nvim/.config/nvim/lua/plugins/php.lua
+++ nvim/.config/nvim/lua/plugins/php.lua
@@ -39,25 +39,25 @@ return {
 				"blade",
 			})
 		end,
-		config = function(_, opts)
-			vim.filetype.add({
-				pattern = {
-					[".*%.blade%.php"] = "blade",
-				},
-			})
-
-			require("nvim-treesitter.configs").setup(opts)
-			---@class ParserInfo[]
-			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-			parser_config.blade = {
-				install_info = {
-					url = "https://github.com/EmranMR/tree-sitter-blade",
-					files = { "src/parser.c" },
-					branch = "main",
-				},
-				filetype = "blade",
-			}
-		end,
+		-- config = function(_, opts)
+		-- 	vim.filetype.add({
+		-- 		pattern = {
+		-- 			[".*%.blade%.php"] = "blade",
+		-- 		},
+		-- 	})
+		--
+		-- 	require("nvim-treesitter.configs").setup(opts)
+		-- 	---@class ParserInfo[]
+		-- 	local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		-- 	parser_config.blade = {
+		-- 		install_info = {
+		-- 			url = "https://github.com/EmranMR/tree-sitter-blade",
+		-- 			files = { "src/parser.c" },
+		-- 			branch = "main",
+		-- 		},
+		-- 		filetype = "blade",
+		-- 	}
+		-- end,
 	},
 	{
 		-- Add the blade-nav.nvim plugin which provides Goto File capabilities
