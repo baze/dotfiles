d0e74e9099db4d579ac62647c0c9569040cb3586
Björn Martensen <b.martensen@zw-systems.de>
2025-03-27 13:14:05 +0100

    replace autosession by using persistence

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -22,3 +22,13 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 -- 		vim.diagnostic.open_float(nil, opts)
 -- 	end,
 -- })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = vim.api.nvim_create_augroup("restore_session", { clear = true }),
+	callback = function()
+		if vim.fn.getcwd() ~= vim.env.HOME then
+			require("persistence").load()
+		end
+	end,
+	nested = true,
+})
