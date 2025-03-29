143faa8e00d3427668ce7de458977ffa34862c52
Björn Martensen <b.martensen@zw-systems.de>
2025-03-29 14:02:30 +0100

    add nvim virtual lines

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -62,3 +62,14 @@ vim.opt.clipboard = "unnamedplus"
 
 vim.opt.virtualedit = "block"
 vim.g.lazyvim_picker = "snacks"
+
+vim.diagnostic.config({
+  -- Use the default configuration
+  virtual_lines = true
+
+  -- Alternatively, customize specific options
+  -- virtual_lines = {
+  --  -- Only show virtual line diagnostics for the current cursor line
+  --  current_line = true,
+  -- },
+})
